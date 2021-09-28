6754979cfa7d7f8167af6010a60d0ebc86fe4df5
reavil <reavil01@gmail.com>
2021-09-28 17:40:22 +0900

    BookCreateRequest 의 멤버변수를 final 로 수정
    - final 인 멤버변수에 대해서는 생성자를 통해 초기화를 요구, 하지만 json -> obj 변환 하려면 기본 생성자를 요구(jackson)하는 문제 발생
    - 생성자 인자에 @JsonProperty 를 붙여주면 기본 생성자(빈 생성자)를 만들지 않아도 json -> object 변환에 문제가 없음

--- src/main/java/com/example/springbootsample/dto/BookCreateRequest.java
+++ src/main/java/com/example/springbootsample/dto/BookCreateRequest.java
@@ -1,17 +1,20 @@
 package com.example.springbootsample.dto;
 
 import com.example.springbootsample.entity.Book;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
 
 public class BookCreateRequest {
-    private String name;
-    private int price;
-    private LocalDate releaseDate;
+    private final String name;
+    private final int price;
+    private final LocalDate releaseDate;
 
-    private BookCreateRequest() {}
-
-    public BookCreateRequest(final String name, final int price, final LocalDate releaseDate) {
+    public BookCreateRequest(
+            @JsonProperty("name") final String name,
+            @JsonProperty("price") final int price,
+            @JsonProperty("releaseDate") final LocalDate releaseDate
+    ) {
         this.name = name;
         this.price = price;
         this.releaseDate = releaseDate;
